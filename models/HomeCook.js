a17da962e33a6c207e20ca773597a895a44afd6b
Donna Engelberger <dengelberger@gmail.com>
2020-11-11 09:41:29 -0500

    extended seedDB.js to include examples, fixed minor models issues

--- models/HomeCook.js
+++ models/HomeCook.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const HomeCookSchema = new Schema({
+	_id: Schema.Types.ObjectId,
 	name: String,
 	created_by: {
 		type: Schema.Types.ObjectId,
