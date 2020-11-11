fb22ea14bf5313b5b07de1cf6d3587b34c654d79
FileOtter <kevin.t.connell@gmail.com>
2020-11-11 17:04:56 -0500

    possible fix

---
+++ passport/passportRoutes.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const passport = require('../passport')
+
+router.post('/api/register', (req, res) => {
+    console.log('user signup');
+
+    const { username, password, first_name, last_name } = req.body
+    // ADD VALIDATION
+    User.findOne({ email: username }, (err, user) => {
+        if (err) {
+            console.log('User.js post error: ', err)
+        } else if (user) {
+            res.json({
+                error: `Sorry, already a user with the email: ${username}`
+            })
+        }
+        else {
+            const newUser = new User({
+                email: username,
+                password: password,
+                first_name: first_name,
+                last_name: last_name
+            })
+            newUser.save((err, savedUser) => {
+                if (err) return res.json(err)
+                res.json(savedUser)
+            })
+        }
+    })
+})
+
+router.post(
+    '/api/login',
+    function (req, res, next) {
+        console.log('routes/user.js, login, req.body: ');
+        console.log(req.body)
+        next()
+    },
+    passport.authenticate('local'),
+    (req, res) => {
+        console.log('logged in', req.user);
+        var userInfo = {
+            username: req.user.username
+        };
+        res.send(userInfo);
+    }
+)
+
+router.get('/api/user', (req, res, next) => {
+    console.log('===== user!!======')
+    console.log(req.user)
+    if (req.user) {
+        res.json({ user: req.user })
+    } else {
+        res.json({ user: null })
+    }
+})
+
+router.post('/api/logout', (req, res) => {
+    if (req.user) {
+        req.logout()
+        res.send({ msg: 'logging out' })
+    } else {
+        res.send({ msg: 'no user to log out' })
+    }
+})
+
+module.exports = router
